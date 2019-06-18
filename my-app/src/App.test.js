86d7652f9893bd8a24b18f8cea104f171dfea3c0
Doug <me@douglasbowen.com>
2019-06-18 16:43:08 -0400

    add dashboard render test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Dashboard from "./components/Dashboard";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("displays dashboard component", () => {
+    const queries = render(<Dashboard />);
+  });
 });
